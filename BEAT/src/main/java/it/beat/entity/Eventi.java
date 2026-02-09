a5d96b9e7476d4a1c4546f8b24ab4dea1fd13504
carmine <c.dimanso@studenti.unisa.it>
2026-02-09 11:11:12 +0100

    new add Evento, mod pom

---
+++ BEAT/src/main/java/it/beat/entity/Eventi.java
@@ -0,0 +1,17 @@
+package it.beat.entity;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+import java.sql.Date;
+@Entity
+@Table(name = "eventi")
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class Eventi {
+
+}
