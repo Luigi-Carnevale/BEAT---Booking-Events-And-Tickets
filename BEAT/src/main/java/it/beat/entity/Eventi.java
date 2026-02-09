b3ecd07e6a86183edf9ba22f3abdc2affd2c879b
carmine <c.dimanso@studenti.unisa.it>
2026-02-09 11:30:06 +0100

    mod Evento

--- BEAT/src/main/java/it/beat/entity/Eventi.java
+++ BEAT/src/main/java/it/beat/entity/Eventi.java
@@ -2,6 +2,7 @@ package it.beat.entity;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
+import java.math.BigInteger;
 import java.sql.Date;
 @Entity
 @Table(name = "eventi")
@@ -13,5 +14,28 @@ import java.sql.Date;
 
 
 public class Eventi {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_evento")
+    private BigInteger idEvento;
+
+    @Column(name = "titolo", nullable = false, length = 100 )
+    private String titolo;
+
+    @Column(name ="descrizione", nullable = true)
+    private String descrizione;
+
+    @Column(name ="protagonista", nullable = false, length = 100)
+    private String protagonista;
+
+    @Column(name = "immagine_url", nullable = false, length = 255)
+    private String immagineUrl;
+
+
+
+
+
+
 
 }
